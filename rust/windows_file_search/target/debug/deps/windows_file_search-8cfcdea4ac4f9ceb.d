da9480c94a318ff54037fc8b78a83957ca036c58
Matthew Heyner <mheyner@gmail.com>
2023-01-22 07:56:00 -0500

    init

---
+++ rust/windows_file_search/target/debug/deps/windows_file_search-8cfcdea4ac4f9ceb.d
@@ -0,0 +1,5 @@
+/home/mheyner/repos/learning-projects/rust/windows_file_search/target/debug/deps/windows_file_search-8cfcdea4ac4f9ceb: src/main.rs
+
+/home/mheyner/repos/learning-projects/rust/windows_file_search/target/debug/deps/windows_file_search-8cfcdea4ac4f9ceb.d: src/main.rs
+
+src/main.rs:
---
+++ rust/windows_file_search/target/debug/windows_file_search.d
@@ -0,0 +1,1 @@
+/home/mheyner/repos/learning-projects/rust/windows_file_search/target/debug/windows_file_search: /home/mheyner/repos/learning-projects/rust/windows_file_search/src/main.rs
